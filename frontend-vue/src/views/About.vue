df2084590ed32d692d72cb6fbcfc5a1293b1f4c1
MadhumithaGanesan <77234429+MadhumithaGanesan@users.noreply.github.com>
2021-03-23 22:42:08 +0530

    Create About.vue

---
+++ frontend-vue/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  <h1>About Us</h1>
+   <p class="text">
+     Luxe Stationery, an e-commerce shopping complex for personal, institutional and office stationery, is a division of Apers India Pvt. Ltd. This virtual stationery market endeavours to bring a wide variety of products at discounted rates. Being into operation since 2011, the company has shaped a broad mount of satisfied consumers from Schools, Universities, Hospitals, Banks, Corporates, NGO’s and many households.
+   </p>
+
+
+  </div>
+</template>
+
